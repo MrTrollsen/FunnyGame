75d4e4cdffd698f019724a035ee4ce8d1c60bed6
Loke <lowenbergpetterssonn.loke@elev.ga.ntig.se>
2023-01-19 10:47:50 +0100

    ,mhljhv

---
+++ FunnyGame/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("welcome to tamagotchi game");
+
+Tamagotchi tamaClone = new Tamagotchi();
+
+Console.WriteLine("Choose name for Tamagotchi");
+tamaClone.name = Console.ReadLine();
+
+Console.WriteLine($"ok! {tamaClone.name} is a mediocre name but thats alright!!");
+
+ (while tamaClone.GetAlive() == true)
+{
+    Console.Clear();
+    tamaClone.PrintStats();
+    Console.WriteLine("now what?");
+    Console.WriteLine($"1. Teach {tamaClone.name} a new term");
+    Console.WriteLine($"2. Talk to {tamaClone.name}");
+    Console.WriteLine($"3. Feed {tamaClone.name}");
+    Console.WriteLine($"4. Do nothing");
+
+  string NowWhat = Console.ReadLine();
+  if (NowWhat == "1")
+  {
+    Console.WriteLine("What word?");
+    string word = Console.ReadLine();
+    tamaClone.Teach(word);
+  }
+  if (NowWhat == "2")
+  {
+    tamaClone.Hi();
+  }
+  if (NowWhat == "3")
+  {
+    tamaClone.Feed();
+  }
+  else
+  {
+    Console.WriteLine("Doing nothing...");
+  }
+  tamaClone.Tick();
+  
+}
+
+Console.WriteLine($"OH NO! {tamaClone.name} is dead!");
+Console.WriteLine("Press ENTER to quit");
+Console.ReadLine();
+
---
+++ FunnyGame/Tamagotchi.cs
@@ -0,0 +1,73 @@
+using System;
+
+namespace FunnyGame;
+public class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private bool isAlive;
+    private Random generator;
+    public string name;
+    private List <string> words = new List<string>() {"hello"};
+
+public Tamagotchi()
+{
+generator = new Random();
+isAlive = true;
+}
+
+public void feed
+{
+    console.WriteLine($" [{name}] eats and becomes less hungry");
+    hunger -= 2;
+
+    if (hunger < 0)
+    {
+        hunger = 0;
+    }
+}
+
+public void Hi()
+  {
+    int wordNum = generator.Next(words.Count);
+    Console.WriteLine($" [{name}] says: {words[wordNum]}");
+    ReduceBoredom();
+  }
+
+  public void Teach(string word)
+  {
+    Console.WriteLine($" [{name}] learns: {word}");
+    words.Add(word);
+    ReduceBoredom();
+  }
+public void Tick()
+  {
+    hunger++;
+    boredom++;
+    if (hunger > 10 || boredom > 10)
+    {
+      isAlive = false;
+    }
+  }
+
+  public void PrintStats()
+  {
+    Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Vocabulary: {words.Count} words");
+  }
+
+  public bool GetAlive()
+  {
+    return isAlive;
+  }
+
+  public void ReduceBoredom()
+  {
+    Console.WriteLine($" [{name}] is now less bored!");
+
+    boredom -= 2;
+    if (boredom < 0)
+    {
+      boredom = 0;
+    }
+  }
+}
